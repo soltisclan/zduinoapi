0cbb48c018af1186602580921ec99e4447a6a090
soltisclan <zoltiz@gmail.com>
2015-09-27 14:14:32 -0400

    Added basic Twitter API.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var Twitter = require('twitter');
+var config = require('./config.js');
 
 function getTime(timeArg) {
     var d  = new Date();
@@ -9,6 +11,18 @@ function getTime(timeArg) {
         dateValue = d;
     }
     return dateValue.toString();
+}
+
+function tweetThis(tweet) {
+    var client = new Twitter(config.twittercreds);
+
+    client.post('statuses/update', { status: tweet }, function (error, tweet, response) {
+        if (error) { throw error; }
+        console.log(tweet);  // Tweet body. 
+        console.log(response);  // Raw response object. 
+    });
+    
+    return "tweeted!";
 }
 
 function processRequest (req, res) {
@@ -23,9 +37,20 @@ function processRequest (req, res) {
             timeArg = reqs[1];
         }
         res.end(getTime(timeArg));
-    } else {
+    } 
+    else if (reqs[0] === "/?tweet"){
+        var tweet="";
+        if (reqs.length > 1) {
+            tweet = reqs[1];
+        }
+        res.end(tweetThis(tweet));        
+    }
+    else 
+    {
         res.end();
     }
 }
+
+
 
 http.createServer(processRequest).listen(process.env.PORT || 8080);
