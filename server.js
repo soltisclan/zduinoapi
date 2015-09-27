808b4e5334717319c0d21131180640d41e99aa8e
soltisclan <zoltiz@gmail.com>
2015-09-27 13:09:34 -0400

    Added Time/Month request API, JSLint

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var http = require('http');
 
-http.createServer(processRequest).listen(process.env.PORT);
+function getTime(timeArg) {
+    var d  = new Date();
+    var dateValue;
+    if (timeArg === "month"){
+        dateValue = d.getMonth() + 1;
+    } else {
+        dateValue = d;
+    }
+    return dateValue.toString();
+}
 
 function processRequest (req, res) {
     console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    var reqs = req.url.split("=");
+
+    if (reqs[0] === '/?time') {
+        var timeArg="";
+        if (reqs.length > 1) {
+            timeArg = reqs[1];
+        }
+        res.end(getTime(timeArg));
+    } else {
+        res.end();
+    }
 }
+
+http.createServer(processRequest).listen(process.env.PORT || 8080);
